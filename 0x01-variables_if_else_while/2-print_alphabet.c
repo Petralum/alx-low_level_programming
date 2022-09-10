0c4b7d266805f2077151b3956bc672d58b635bc9
‘Petralum’ <petralum941@yahoo.com>
2022-09-09 20:55:18 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * return: Always 0 (Success)
+  */
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+	putchar(a);
+	}
+
+	retun (0);
+}
