f88e0c4e0a7a1fafb0785668900236f9885a303d
‘Petralum’ <petralum941@yahoo.com>
2022-09-11 08:57:03 -0700

    Combination of 2 digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,22 +6,21 @@
   * Return: Always 0 (Success)
   */
 
-  int main(void)
+int main(void)
 {
 	int num;
 
-	for ( num = 0; num <= 9; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		putchar(num % 10); + '0');
+		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
 
 			putchar(',');
 			putchar(' ');
 	}
-	
+
 	putchar('\n');
 
 	return (0);
 }
-
