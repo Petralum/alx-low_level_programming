a82903240c00857964eb53b016275c85a56c0b2a
‘Petralum’ <petralum941@yahoo.com>
2022-09-12 23:21:12 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,18 @@
   *
   * Return: Always 0 (Success)
   */
-void print_alphabet(void);
+void print_alphabet_x10(void);
 int main(void)
 {
 	int ch;
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
-	putchar('\n');
+	 putchar('\n');
 
 	return (0);
 }
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int alphabet;
 }
