f4c5559d8fb0bc2be9f2c89f19c1c43ab3a5e1d1
‘Petralum’ <petralum941@yahoo.com>
2022-09-12 23:02:23 -0700

    aphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - Prints alphabet in lowercase
+  *
+  * Return: Always 0 (Success)
+  */
+void print_alphabet(void);
+int main(void)
+{
+	int ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	int alphabet;
+	putchar(alphabet);
+}
