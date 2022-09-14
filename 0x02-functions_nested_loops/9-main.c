65dee5ce8287f47b736407d43cdba954b5162ea5
‘Petralum’ <petralum941@yahoo.com>
2022-09-14 13:15:49 -0700

    98

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * print_times_table - prints the times table for n
+  *@n: The multiplication table requested
+  * Return: Nothing
+  */
+int main(void)
+{
+	times_table();
+	return (0);
+}
