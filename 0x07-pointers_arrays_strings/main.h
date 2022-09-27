00380e29666390559fbfed89f727708a4ba39354
‘Petralum’ <petralum941@yahoo.com>
2022-09-26 23:26:26 -0700

    memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-fndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 char *_memset(char *s, char b, unsigned int n);
@@ -9,6 +9,5 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-int _putchar(char c)
 
-#endif
+#endif /*MAIN_H*/
