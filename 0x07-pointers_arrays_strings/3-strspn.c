de853cd599b479f4b224c925007e51fe10886a65
‘Petralum’ <petralum941@yahoo.com>
2022-09-27 16:03:09 -0700

    print diagsums

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,19 +2,20 @@
 
 /**
   * _strspn - gets the length of a prefix substring
-  * @s - string to check
-  * @accept - string to check against
+  * @s: string to check
+  * @accept: string to check against
   *
   * Return: number of bytes of s in accept
   */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
+	unsigned int i;
+        unsigned int j;
 
-	for (i = 0; s[i]; i++;
+	for (i = 0; s[i]; i++)
 	{
-		for (j = 0; accept[j]; j++;
+		for (j = 0; accept[j]; j++)
 		{
 			if s[i] == accept[j]
 				break;
