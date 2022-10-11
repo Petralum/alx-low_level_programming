ac24d85d7522224252f4798fcbf8939eadb7c092
‘Petralum’ <petralum941@yahoo.com>
2022-10-11 07:24:39 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - Entry point
+  * @name: dog name
+  * @age: Age of dog
+  * @owner: Owner of dog
+  *
+  * Return: Always 0 on success
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
